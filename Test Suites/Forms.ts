d2d1be450d933fa3506b86cbd5ae17a56e0bb890
IlhamWA <ilhamwafiq107@gmail.com>
2025-06-18 23:59:13 +0700

    penambahan test suites

---
+++ Test Suites/Forms.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Forms</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>090eb345-b065-4167-8dbf-da5a2527de03</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c35256e-7e31-485c-b281-e49d306ccbfe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fill Forms/TC06 - Form</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
